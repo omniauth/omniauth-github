567c2f89ae346f3bcf36aae538b12ac1e29fe0f1
Tom Milewski <tmilewski@gmail.com>
2016-08-07 00:13:11 -0400

    Basic standardization of specs

--- spec/omniauth/strategies/github_spec.rb
+++ spec/omniauth/strategies/github_spec.rb
@@ -28,9 +28,9 @@ describe OmniAuth::Strategies::GitHub do
     allow(subject).to receive(:access_token).and_return(access_token)
   end
 
-  context "client options" do
+  context 'client options' do
     it 'should have correct site' do
-      expect(subject.options.client_options.site).to eq("https://api.github.com")
+      expect(subject.options.client_options.site).to eq('https://api.github.com')
     end
 
     it 'should have correct authorize url' do
@@ -41,60 +41,60 @@ describe OmniAuth::Strategies::GitHub do
       expect(subject.options.client_options.token_url).to eq('https://github.com/login/oauth/access_token')
     end
 
-    describe "should be overrideable" do
-      it "for site" do
+    describe 'should be overrideable' do
+      it 'for site' do
         expect(enterprise.options.client_options.site).to eq(enterprise_site)
       end
 
-      it "for authorize url" do
+      it 'for authorize url' do
         expect(enterprise.options.client_options.authorize_url).to eq(enterprise_authorize_url)
       end
 
-      it "for token url" do
+      it 'for token url' do
         expect(enterprise.options.client_options.token_url).to eq(enterprise_token_url)
       end
     end
   end
 
-  context "#email_access_allowed?" do
-    it "should not allow email if scope is nil" do
+  context '#email_access_allowed?' do
+    it 'should not allow email if scope is nil' do
       expect(subject.options['scope']).to be_nil
       expect(subject).to_not be_email_access_allowed
     end
 
-    it "should allow email if scope is user" do
+    it 'should allow email if scope is user' do
       subject.options['scope'] = 'user'
       expect(subject).to be_email_access_allowed
     end
 
-    it "should allow email if scope is a bunch of stuff including user" do
+    it 'should allow email if scope is a bunch of stuff including user' do
       subject.options['scope'] = 'public_repo,user,repo,delete_repo,gist'
       expect(subject).to be_email_access_allowed
     end
 
-    it "should not allow email if scope does not grant email access" do
+    it 'should not allow email if scope does not grant email access' do
       subject.options['scope'] = 'repo,user:follow'
       expect(subject).to_not be_email_access_allowed
     end
 
-    it "should assume email access not allowed if scope is something currently not documented " do
+    it 'should assume email access not allowed if scope is something currently not documented' do
       subject.options['scope'] = 'currently_not_documented'
       expect(subject).to_not be_email_access_allowed
     end
   end
 
-  context "#email" do
-    it "should return email from raw_info if available" do
-      allow(subject).to receive(:raw_info).and_return({'email' => 'you@example.com'})
+  context '#email' do
+    it 'should return email from raw_info if available' do
+      allow(subject).to receive(:raw_info).and_return({ 'email' => 'you@example.com' })
       expect(subject.email).to eq('you@example.com')
     end
 
-    it "should return nil if there is no raw_info and email access is not allowed" do
+    it 'should return nil if there is no raw_info and email access is not allowed' do
       allow(subject).to receive(:raw_info).and_return({})
       expect(subject.email).to be_nil
     end
 
-    it "should not return the primary email if there is no raw_info and email access is allowed" do
+    it 'should not return the primary email if there is no raw_info and email access is allowed' do
       emails = [
         { 'email' => 'secondary@example.com', 'primary' => false },
         { 'email' => 'primary@example.com',   'primary' => true }
@@ -105,7 +105,7 @@ describe OmniAuth::Strategies::GitHub do
       expect(subject.email).to be_nil
     end
 
-    it "should not return the first email if there is no raw_info and email access is allowed" do
+    it 'should not return the first email if there is no raw_info and email access is allowed' do
       emails = [
         { 'email' => 'first@example.com',   'primary' => false },
         { 'email' => 'second@example.com',  'primary' => false }
@@ -117,17 +117,17 @@ describe OmniAuth::Strategies::GitHub do
     end
   end
 
-  context "#raw_info" do
-    it "should use relative paths" do
+  context '#raw_info' do
+    it 'should use relative paths' do
       expect(access_token).to receive(:get).with('user').and_return(response)
       expect(subject.raw_info).to eq(parsed_response)
     end
   end
 
-  context "#emails" do
-    it "should use relative paths" do
+  context '#emails' do
+    it 'should use relative paths' do
       expect(access_token).to receive(:get).with('user/emails', :headers => {
-        "Accept" => "application/vnd.github.v3"
+        'Accept' => 'application/vnd.github.v3'
       }).and_return(response)
 
       subject.options['scope'] = 'user'
