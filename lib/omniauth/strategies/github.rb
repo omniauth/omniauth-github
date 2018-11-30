17be3c65feca80aae7fee0683e860f5ad479a270
Marcus Guttenplan <marcusxhim@gmail.com>
2018-11-29 19:51:46 -0800

    update omniauth strategy to grab locations

--- lib/omniauth/strategies/github.rb
+++ lib/omniauth/strategies/github.rb
@@ -31,7 +31,7 @@ module OmniAuth
           'email' => email,
           'name' => raw_info['name'],
           'image' => raw_info['avatar_url'],
-          'location' => raw_info['location']
+          'location' => raw_info['location'],
           'urls' => {
             'GitHub' => raw_info['html_url'],
             'Blog' => raw_info['blog'],
